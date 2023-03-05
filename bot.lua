4dc266f54a9d73c7cc09017d986a2ff10f140a3f
Noé Falzon <1331434+Castux@users.noreply.github.com>
2023-03-05 13:03:11 +0000

    Logging. Status, start/stop phase.

--- bot.lua
+++ bot.lua
@@ -2,6 +2,8 @@ local discordia = require('discordia')
 local json = require('json')
 local config = dofile('config.lua')
 
+--[[ Setup ]]
+
 local client = discordia.Client()
 
 local data = {}
@@ -28,34 +30,78 @@ end
 local function saveData()
 	local fp = io.open(config.data, "w")
 	fp:write(json.encode(data))
-
-	print("Wrote data to " .. config.data)
+	fp:close()
 end
 
 local function isPrivate(message)
 	return message.channel.type == discordia.enums.channelType.private
 end
 
+local function reply(message, ...)
+	message.channel:send(string.format(...))
+end
+
+--[[ Commands ]]
+
+local commands = {}
+
+function commands.ping(message, payload)
+	reply(message, "Pong! " .. payload)
+end
+
+function commands.status(message)
+
+	if data.phase then
+
+		local count = 0
+		for k,v in pairs(data[data.phase]) do
+			count = count + 1
+		end
+
+		reply(message, "Accepting orders for **" .. data.phase .. "**.")
+
+	else
+		reply(message, "Not accepting orders at the moment")
+	end
+	
+end
+
+function commands.startphase(message, payload)
+
+	if not payload or #payload == 0 then
+		reply(message, "Invalid phase")
+		return
+	end
+
+	data.phase = payload
+	data[payload] = {}
+	reply(message, "Starting phase **" .. payload .. "**")
+end
+
+function commands.stopphase(message, payload)
+	data.phase = nil
+	reply(message, "Stopped current phase")
+end
+
+--[[ Bot start ]]
+
 client:on('ready', function()
+
 	print('Logged in as '.. client.user.username)
 	loadData()
 end)
 
-local commands = {
-
-	ping = function(message)
-		message.channel:send("Pong!")
-	end
-
-}
-
 client:on('messageCreate', function(message)
-	
+
 	for com, func in pairs(commands) do
-		if message.content:lower():match("^" .. config.prefix .. com) then
-			func(message)
+		local trigger,payload = message.content:match("^(" .. config.prefix .. com .. ")%s*(.*)")
+		if trigger then
+			print(message.timestamp, message.author.username, com, payload)
+			func(message, payload)
+			saveData()
 		end
 	end
+
 end)
 
 client:run('Bot ' .. config.token)
