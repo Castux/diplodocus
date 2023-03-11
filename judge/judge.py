76eacbf69cb253968ba463f0c1f3e7147effa557
Noé Falzon <1331434+Castux@users.noreply.github.com>
2023-03-11 14:27:13 +0200

    Starting the python bot

--- judge/judge.py
+++ judge/judge.py
@@ -1,29 +1,65 @@
 import random
 import json
 import sys
+import discord
+from discord.ext import commands
 
 sys.path.append('diplomacy')
 
 from diplomacy import Game
 from diplomacy.utils.export import to_saved_game_format
 
-game = Game(map_name='chasers.map')
-
-#print(game.map.convoy_paths[10])
-
-game.set_units('EMPIRE', ['A ANA', 'F IMP', 'F CHA'])
-game.process()
+# game = Game(map_name='chasers.map')
+# game.process()
 #
-# orders =  game.get_all_possible_orders()
-# for x in orders:
-#     print(x, orders[x])
+# # print(game.map.convoy_paths[10])
+#
+# game.set_units('EMPIRE', ['F ANA', 'F TIT'])
+#
+# game.set_orders("EMPIRE", [
+#     'F ANA - RIV',
+#     'F TIT C F ANA - RIV'
+# ])
+#
+# for err in game.error:
+#     print(err)
+#
+# print(game.get_orders())
+# game.process()
+# print(game.get_order_status())
 
-game.set_orders("EMPIRE", [
-    'A ANA - HOW',
-    'F IMP C A ANA - HOW'
-])
-
-game.process()
-print(game.get_state())
 
 #to_saved_game_format(game, output_path='game.json')
+
+def load_config(path):
+    with open(path, "r") as f:
+        return json.load(f)
+
+def start_bot(config):
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix=config["prefix"], intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f'Logged on as {bot.user}: {bot.user.id}!')
+
+    @bot.command()
+    async def ping(ctx, content):
+        await ctx.send("Pong! " + content or "")
+
+    @bot.group()
+    async def cool(ctx):
+        """Says if a user is cool.
+        In reality this just checks if a subcommand is being invoked.
+        """
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+    bot.run(config["token"])
+
+
+config = load_config("config.json")
+start_bot(config)
