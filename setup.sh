03ef859eb545c78108d95b93fd5c3b12cd0cb3fd
Noé Falzon <1331434+Castux@users.noreply.github.com>
2023-03-13 00:30:16 +0200

    Extra setup steps

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install python3-venv python3-pip
 python3 -m pip install -U discord.py
+
+git submodule init
+git submodule update
+
+cd diplomacy
+pip install -r requirements.txt
